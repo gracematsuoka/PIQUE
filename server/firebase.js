e9ece21e9c3f81ee604897a90199806b91242248
Grace Matsuoka <gracelmatsuoka@gmail.com>
2025-06-11 15:12:40 -0700

    updated firebase

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,18 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+    credential: admin.credential.cert({
+      type: "service_account",
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: "https://accounts.google.com/o/oauth2/auth",
+      tokenUri: "https://oauth2.googleapis.com/token",
+      authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+      clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL
+    })
+  });
 
 module.exports = admin;
